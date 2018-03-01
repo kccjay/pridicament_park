e64869b2ffe763564aef720bf49a04009b089116
kccjay <cchave3462@greenvilleschools.us>
2018-03-01 09:35:20 -0500

    Add files via upload

--- predicament_park.py
+++ predicament_park.py
@@ -22,8 +22,29 @@ refresh_rate = 60
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREEN = (0, 128, 43)
+GREY = (191, 191, 191)
 
+# Fonts
+GAME_FONT = pygame.font.Font(None, 50)
+
+# stages
+START = 0
+PLAYING = 1
+END = 2
+
+def setup():
+    global block_pos, block_vel, size, stage, time_remaining, ticks
     
+    block_pos = [375, 275]
+    block_vel = [0, 0]
+    size = 50
+
+    stage = START
+    time_remaining = 10
+    ticks = 0
+
+
 # Make a block
 block =  [60, 60, 40, 40]
 vel = [0, 0]
@@ -104,70 +125,100 @@ walls = [wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9, wall10,
          wall11, wall12, wall13, wall14, wall15, wall16, wall17, wall18, wall19, wall20,
          wall21, wall22, wall23, wall24, wall25, wall26, wall27, wall28, wall29, wall30,
          wall31]
-
+stage = START
 # Game loop
 done = False
 
 while not done:
     # Event processing (React to key presses, mouse clicks, etc.)
     ''' for now, we'll just check to see if the X is clicked '''
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.KEYDOWN:
 
+            if stage == START:
+                if event.key == pygame.K_SPACE:
+                    stage = PLAYING            
+                    
+            elif stage == END:
+                if event.key == pygame.K_SPACE:
+                    setup()
+
+                    
     state = pygame.key.get_pressed()
-
+    
     up = state[pygame.K_UP]
     down = state[pygame.K_DOWN]
     left = state[pygame.K_LEFT]
     right = state[pygame.K_RIGHT]
+            
 
-    if left:
-        vel[0]  = -speed
-    elif right:
-        vel[0]  = speed
-    else:
-        vel[0]  = 0
 
-    if up:
-        vel[1] = -speed
-    elif down:
-        vel[1]  = speed
-    else:
-        vel[1]  = 0
-        
-        
+    if stage == PLAYING:                
+        if left:
+            vel[0]  = -speed
+        elif right:
+            vel[0]  = speed
+        else:
+            vel[0]  = 0
+
+        if up:
+            vel[1] = -speed
+        elif down:
+            vel[1]  = speed
+        else:
+            vel[1]  = 0
+                    
+                
     # Game logic (Check for collisions, update points, etc.)
-    ''' move the block in horizontal direction '''
-    block[0] += vel[0]
+    if stage == PLAYING:
+        ''' move the block in horizontal direction '''
+        block[0] += vel[0]
 
-    ''' resolve collisions '''
-    for w in walls:
-        if intersects.rect_rect(block, w):        
-            if vel[0]> 0:
-                block[0] = w[0] - block[2]
-            elif vel[0] < 0:
-                block[0] = w[0] + w[2]
+        ''' resolve collisions '''
+        for w in walls:
+            if intersects.rect_rect(block, w):        
+                if vel[0]> 0:
+                    block[0] = w[0] - block[2]
+                elif vel[0] < 0:
+                    block[0] = w[0] + w[2]
 
-    ''' move the block in vertical direction '''
-    block[1] += vel[1]
-    
-    ''' resolve collisions '''
-    for w in walls:
-        if intersects.rect_rect(block, w):                    
-            if vel[1] > 0:
-                block[1] = w[1] - block[3]
-            if vel[1] < 0:
-                block[1] = w[1] + w[3]
+        ''' move the block in vertical direction '''
+        block[1] += vel[1]
+        
+        ''' resolve collisions '''
+        for w in walls:
+            if intersects.rect_rect(block, w):                    
+                if vel[1] > 0:
+                    block[1] = w[1] - block[3]
+                if vel[1] < 0:
+                    block[1] = w[1] + w[3]
+
+                    
+    ''' this controls the timer '''
+    if stage == PLAYING:
+        ticks += 1
+
+        if ticks % refresh_rate == 0:
+            time_remaining -= 1
+
+        if time_remaining == 0:
+            stage = END
 
     
     # Drawing code (Describe the picture. It isn't actually drawn yet.)
-    screen.fill(BLACK)
+    screen.fill(GREY)
 
-    pygame.draw.rect(screen, WHITE, block)
+    pygame.draw.rect(screen, BLACK, block)
+    
+    ''' displays the timer '''
+    timer_text = GAME_FONT.render(str(time_remaining), True, WHITE)
+    screen.blit(timer_text, [560, 380])
 
     for w in walls:
-        pygame.draw.rect(screen, RED, w)
+        pygame.draw.rect(screen, GREEN, w)
 
     # Update screen (Actually draw the picture in the window.)
     pygame.display.flip()
